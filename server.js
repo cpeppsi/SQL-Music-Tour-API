cdc1b681c127f6b9e7b0a717b38ff8d242ec0954
cpeppsi <caepps28@gmail.com>
2023-10-21 01:27:03 -0400

    Established connection to  Sequelize

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 // DEPENDENCIES
 const express = require('express')
+const { Sequelize } = require('sequelize')
+
 const app = express()
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+// // SEQUELIZE CONNECTION
+// const sequelize = new Sequelize({
+//     storage: process.env.PG_URI,
+//     dialect: 'postgres',
+//     username: 'postgres',
+//     password: 'my_password'
+//   })
+
+try {
+    sequelize.authenticate()
+    console.log(`Happily connected with Sequelize at ${process.env.PG_URI}`)
+} catch(err) {
+    console.log(`Cannot connect to PG: ${err}`)
+}
 
 // ROOT
 app.get('/', (req, res) => {
