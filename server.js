1e423b77f79b96219279fa1319277a42923ea0b8
cpeppsi <caepps28@gmail.com>
2023-10-25 10:31:10 -0400

    Created event and band models/tables

--- server.js
+++ server.js
@@ -9,23 +9,23 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// SEQUELIZE CONNECTION
-const sequelize = new Sequelize(process.env.PG_URI)
-
 // // SEQUELIZE CONNECTION
-// const sequelize = new Sequelize({
-//     storage: process.env.PG_URI,
-//     dialect: 'postgres',
-//     username: 'postgres',
-//     password: 'my_password'
-//   })
+// const sequelize = new Sequelize(process.env.PG_URI)
 
-try {
-    sequelize.authenticate()
-    console.log(`Happily connected with Sequelize at ${process.env.PG_URI}`)
-} catch(err) {
-    console.log(`Cannot connect to PG: ${err}`)
-}
+// try {
+//     sequelize.authenticate()
+//     console.log(`Happily connected with Sequelize at ${process.env.PG_URI}`)
+// } catch(err) {
+//     console.log(`Cannot connect to PG: ${err}`)
+// }
+
+// // // SEQUELIZE CONNECTION
+// // const sequelize = new Sequelize({
+// //     storage: process.env.PG_URI,
+// //     dialect: 'postgres',
+// //     username: 'postgres',
+// //     password: 'my_password'
+// //   })
 
 // ROOT
 app.get('/', (req, res) => {
