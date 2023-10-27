14a9173c248a45ef5dadc5ee6e906e47ea5b54c2
cpeppsi <caepps28@gmail.com>
2023-10-26 21:13:27 -0400

    Adding foreign keys through models versus migrations

--- models/stage.js
+++ models/stage.js
@@ -9,8 +9,17 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Event, StageEvent, Set_Time }) {
       // define association here
+      Stage.belongsToMany(Event, {
+        foreignKey: "stage_id",
+        as: "events",
+        through: StageEvent
+      })
+      Stage.hasMany(Set_Time, {
+        foreignKey: "stage_id",
+        as: "set_times"
+      })
     }
   }
   Stage.init({
